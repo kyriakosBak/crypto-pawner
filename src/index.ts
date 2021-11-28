e08333f279576879353bdda65c68292fe59b6e55
Kyriakos <10158477+kyriakosBak@users.noreply.github.com>
2021-11-28 09:50:02 +0100

    Improving how transactions are generated. Code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Interface } from '@ethersproject/abi';
 import { TransactionRequest } from '@ethersproject/providers';
-import { FlashbotsBundleProvider, FlashbotsBundleResolution } from '@flashbots/ethers-provider-bundle';
+import { FlashbotsBundleProvider, FlashbotsBundleRawTransaction, FlashbotsBundleResolution, FlashbotsBundleTransaction } from '@flashbots/ethers-provider-bundle';
 import axios from 'axios';
 import dotenv from 'dotenv';
 import { BigNumber, providers, Wallet } from 'ethers';
@@ -10,9 +10,13 @@ const GWEI = BigNumber.from(10).pow(9)
 const ETH = BigNumber.from(10).pow(18)
 
 dotenv.config()
-const NFT_VALUE = ETH.div(100).mul(8)
-const NFT_PIECES = BigNumber.from(process.env.NFT_PIECES)
-const MINER_BRIBE_GWEI = GWEI.mul(BigNumber.from(process.env.MINER_BRIBE_GWEI))
+const IS_PROD = process.env.IS_PROD ? process.env.IS_PROD === "true" : false
+const NFT_PRICE_ETH = process.env.NFT_PRICE_ETH ? ETH.mul(Number(process.env.NFT_PRICE_ETH) * 10000).div(10000) : BigNumber.from(0)
+const NFT_PIECES = Number(process.env.NFT_PIECES) // Total:per-mint OR Total (automatically 1 per mint)
+const NFT_PIECES_PER_MINT = Number(process.env.NFT_PIECES_PER_MINT)
+const MINER_BRIBE_GWEI = process.env.MINER_BRIBER_GWEI
+    ? GWEI.mul(BigNumber.from(process.env.MINER_BRIBE_GWEI))
+    : GWEI.mul(2).add(1)  // 2.000000001 GWEI
 
 const NFT_ADDRESS = process.env.NFT_ADDRESS
 const MINT_DATA = process.env.MINT_DATA
@@ -21,13 +25,16 @@ const ETHERSCAN_ENDPOINT = process.env.ETHERSCAN_ENDPOINT ? process.env.ETHERSCA
 const ETHERSCAN_TOKEN = process.env.ETHERSCAN_TOKEN
 const CHAIN_ID = process.env.CHAIN_ID ? Number(process.env.CHAIN_ID) : 5
 const BLOCKS_IN_FUTURE = process.env.BLOCKS_IN_FUTURE ? Number(process.env.BLOCKS_IN_FUTURE) : 1
-const provider = new providers.InfuraProvider(CHAIN_ID) // TODO - set infura token
+const provider = new providers.InfuraProvider(CHAIN_ID, process.env.INFURA_TOKEN)
 
 // wallet keys
 const FLASBHOTS_PRIVATE_KEY = process.env.FLASBHOTS_PRIVATE_KEY
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY
 
-function runENVChecks() {
+let authSigner: Wallet
+let wallet: Wallet
+
+function runChecksAndInit() {
 
     if (FLASBHOTS_PRIVATE_KEY == undefined) {
         console.error("Flasbhots key undefined. Exiting")
@@ -48,13 +55,16 @@ function runENVChecks() {
         console.error("NFT Contract undefined. Exiting.")
         process.exit(1)
     }
+
+    authSigner = new Wallet(FLASBHOTS_PRIVATE_KEY)
+    wallet = new Wallet(WALLET_PRIVATE_KEY, provider)
 }
 
 function getTransaction(txdata: string, maxBaseFee: BigNumber): TransactionRequest {
     return {
         chainId: CHAIN_ID,
         type: 2,
-        value: NFT_VALUE,
+        value: NFT_PRICE_ETH,
         data: txdata,
         maxFeePerGas: maxBaseFee.add(MINER_BRIBE_GWEI),
         maxPriorityFeePerGas: MINER_BRIBE_GWEI, // max priority fee == bribe
@@ -72,92 +82,95 @@ async function getNFTMintData(nftAdress: string | undefined): Promise<string> {
             if (element.hasOwnProperty("name") && element["name"].toLowerCase().includes("mint") && element.hasOwnProperty("stateMutability") && element["stateMutability"] === "payable") {
                 const iface = new Interface(jsonAbi)
                 const functionData = iface.getSighash(element["name"])
-
-                // TODO - Handle case where there is no inputs and we can only mint 1
-
                 // if there is only one input in the mint funciton, we assume it's the number of tokens we want to buy
-                if (element.hasOwnProperty("inputs") && !element.hasOwnProperty(1) && element["inputs"][0]["type"].includes('int')) {
-                    // We get number of bit and divide by four to get the hex string that needs to be sent
-                    const hexDigits: number = element["inputs"][0]["type"].replace('uint', '').replace('int', '') / 4
-                    let hexData: string = NFT_PIECES.toString()
-                    while (hexData.length < hexDigits) {
-                        hexData = '0' + hexData
+                if (element.hasOwnProperty("inputs") && element["inputs"].length !== 0) {
+                    if (!element["inputs"].hasOwnProperty(1) && element["inputs"][0]["type"].includes('int')) {
+                        // We get number of bit and divide by four to get the hex string that needs to be sent
+                        const hexDigits: number = element["inputs"][0]["type"].replace('uint', '').replace('int', '') / 4
+                        let hexData: string = NFT_PIECES.toString()
+                        while (hexData.length < hexDigits) {
+                            hexData = '0' + hexData
+                        }
+                        return functionData + hexData
                     }
-                    return functionData + hexData
+                    else
+                        throw new Error("Cannot figure out txData. User needs to provide it in manually. Aborting");
                 }
                 else {
-                    throw new Error("Cannot figure out txData. User need to give it in manually. Aborting");
+                    return functionData
                 }
             }
         }
     } catch (exception) {
-        process.stderr.write(`ERROR received from ${url}: ${exception}\n`);
+        console.log(exception)
     }
     console.log("Could not find mint function.")
     return ""
 }
 
 async function main() {
-    // Init wallets
-    const authSigner = new Wallet(FLASBHOTS_PRIVATE_KEY)
-    if (WALLET_PRIVATE_KEY == undefined)
-        return
-    const wallet = new Wallet(WALLET_PRIVATE_KEY, provider)
-
     // Start Flashbots provider and get mint function data
     const flashbotsProvider = await FlashbotsBundleProvider.create(provider, authSigner, FLASHBOTS_ENDPOINT)
-    const transactionData = MINT_DATA ? MINT_DATA : await getNFTMintData(NFT_ADDRESS)
-    if (transactionData === '') { return }
+    const transactionMintData = MINT_DATA ? MINT_DATA : await getNFTMintData(NFT_ADDRESS)
+    if (transactionMintData === '') { return }
 
-    // Start executing for each block
-    provider.on('block', async (blockNumber) => {
-        console.log(blockNumber)
-        const block = await provider.getBlock(blockNumber)
-        const maxBaseFeeInFutureBlock = FlashbotsBundleProvider.getMaxBaseFeeInFutureBlock(BigNumber.from(block.baseFeePerGas), BLOCKS_IN_FUTURE)
-        const tx = getTransaction(transactionData, maxBaseFeeInFutureBlock)
-        const bundledTransaction =
-            [
-                {
-                    transaction: getTransaction(transactionData, maxBaseFeeInFutureBlock),
-                    signer: wallet
-                },
-                {
-                    transaction: getTransaction(transactionData, maxBaseFeeInFutureBlock),
-                    signer: wallet
-                }
-            ]
-        const signedBundle = await flashbotsProvider.signBundle(bundledTransaction)
-        const simulation = await flashbotsProvider.simulate(signedBundle, blockNumber + BLOCKS_IN_FUTURE)
-        if ('error' in simulation) {
-            console.warn(`Simulation Error: ${simulation.error.message}`)
-            process.exit(1)
-        } else {
-            console.log(`Simulation Success: ${JSON.stringify(simulation, null, 2)}`)
+    const blockNumber = await provider.getBlockNumber()
+    const block = await provider.getBlock(blockNumber)
+    const maxBaseFeeInFutureBlock = FlashbotsBundleProvider.getMaxBaseFeeInFutureBlock(BigNumber.from(block.baseFeePerGas), BLOCKS_IN_FUTURE)
+
+    // Define multiple contracts or just a big one
+    let bundledTransaction: (FlashbotsBundleTransaction | FlashbotsBundleRawTransaction)[] = []
+    const contractsToMint = Number(NFT_PIECES) / Number(NFT_PIECES_PER_MINT)
+    for (let index = 0; index < contractsToMint; index++) {
+        bundledTransaction.push(
+            {
+                transaction: getTransaction(transactionMintData, maxBaseFeeInFutureBlock),
+                signer: wallet
+            }
+        )
+    }
+    const signedBundle = await flashbotsProvider.signBundle(bundledTransaction)
+    const simulation = await flashbotsProvider.simulate(signedBundle, blockNumber + BLOCKS_IN_FUTURE)
+    if ('error' in simulation) {
+        console.warn(`Simulation Error: ${simulation.error.message}`)
+        process.exit(1)
+    } else {
+        console.log(`Simulation Success: ${JSON.stringify(simulation, null, 2)}`)
+    }
+
+    const balanceBefore = await wallet.getBalance()
+    for (let block = 1; block < blockNumber + BLOCKS_IN_FUTURE; block++) {
+        let targetBlock = block + blockNumber
+        console.log("Sending transaction for block " + targetBlock)
+        const bundleSubmission = await flashbotsProvider.sendBundle(bundledTransaction, targetBlock)
+        if ('error' in bundleSubmission) {
+            console.warn(bundleSubmission.error.message)
+            throw new Error(bundleSubmission.error.message);
         }
 
-        const balanceBefore = await wallet.getBalance()
-        for (let block = 1; block < 10; block++) {
-            const bundleSubmission = await flashbotsProvider.sendBundle(bundledTransaction, blockNumber + block)
-            if ('error' in bundleSubmission) {
-                console.warn(bundleSubmission.error.message)
-                throw new Error(bundleSubmission.error.message);
-            }
-            const waitResponse = await bundleSubmission.wait()
-            console.log(`Wait Response: ${FlashbotsBundleResolution[waitResponse]}`)
+        const waitResponse = await bundleSubmission.wait()
+        console.log(`Wait Response: ${FlashbotsBundleResolution[waitResponse]}`)
 
-            if (waitResponse === FlashbotsBundleResolution.BundleIncluded || waitResponse === FlashbotsBundleResolution.AccountNonceTooHigh) {
-                process.exit(0)
-            } else {
+        if (waitResponse === FlashbotsBundleResolution.BundleIncluded) {
+            console.log("Bundle included in block " + targetBlock)
+            process.exit(0)
+        }
+        else if (waitResponse === FlashbotsBundleResolution.AccountNonceTooHigh) {
+            console.log("Account nonce too high.")
+            process.exit(1)
+        }
+        else {
+            if (IS_PROD) {
                 console.log({
-                    //bundleStats: await flashbotsProvider.getBundleStats(simulation.bundleHash, blockNumber + BLOCKS_IN_FUTURE),
-                    //userStats: await flashbotsProvider.getUserStats()
+                    bundleStats: await flashbotsProvider.getBundleStats(simulation.bundleHash, targetBlock),
+                    userStats: await flashbotsProvider.getUserStats()
                 })
             }
         }
-        console.log('Balance lost = ' + balanceBefore.sub(await wallet.getBalance()).toString())
-        exit(1)
-    })
+    }
+    console.log('Balance lost in ether = ' + balanceBefore.sub(await wallet.getBalance()).div(ETH).toString())
+    exit(1)
 }
 
-runENVChecks();
+runChecksAndInit();
 main();
